06f817d88439bd791183b121f77985fe761f8a0f
amnazu <amna_zukic@hotmail.com>
2021-03-30 23:22:20 +0200

    Dvadeset treći dan

---
+++ ex02/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Plant from './Plant.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Plant />
+    </div>
+  );
+}
+
+export default App;
